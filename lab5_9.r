337f42c5ca342382eafb0369c8c2f64453a941b6
lythx <lythxxx@gmail.com>
2024-12-12 19:12:01 +0100

    lab 5 9

--- lab5_9.r
+++ lab5_9.r
@@ -23,16 +23,16 @@ for (i in 1:nrow(distribution)) {
   marginal_dist[row[2] + 1, 2] = marginal_dist[row[2] + 1, 2] + row[4]
   marginal_dist[row[3] + 1, 3] = marginal_dist[row[3] + 1, 3] + row[4]
 }
-# 
-# cov_j_g = cov(distribution[,1], distribution[,2])
-# cov_j_s = cov(distribution[,1], distribution[,3])
-# cov_g_s = cov(distribution[,2], distribution[,3])
+
+cov_j_g = cov(marginal_dist[,1], marginal_dist[,2])
+cov_j_s = cov(marginal_dist[,1], marginal_dist[,3])
+cov_g_s = cov(marginal_dist[,2], marginal_dist[,3])
 
 colnames(distribution) = c("jabłka", "gruszki", "śliwki", "Pr")
 print(distribution)
 colnames(marginal_dist) = c("jabłka", "gruszki", "śliwki")
-rownames(marginal_dist) = 1:6
+rownames(marginal_dist) = 0:5
 print(marginal_dist)
-# print(cov_j_g)
-# print(cov_j_s)
-# print(cov_g_s)
+print(c("Cov(jabłko, gruszka)", cov_j_g))
+print(c("Cov(jabłko, śliwka)", cov_j_s))
+print(c("Cov(gruszka, śliwka)", cov_g_s))
